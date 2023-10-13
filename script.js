f778d0157188e35450ba331394a3cb9f6a0e4017
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-13 16:20:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+// komentariya
+
+console.log("Bu mening sahifam");
+
+// console.log("Lorem ipsum dolor sit amet consectetur adipisicing elit Hic porro autem nesciunt nulla
+// at facilis, velit 
+// fugiat cupiditate beatae modi tempora quaerat placeat dicta dignissimos deserunt natus culpa aperiam 
+// reiciendis earum quisquam optio asperiores eos nostrum. Eos vel soluta tenetur quis est aut temporibus
+//  obcaecati in commodi et! Porro corporis expedita quisquam voluptatem et blanditiis sit velit voluptate 
+//  odit commodi? Odit repellat rerum eum deleniti, incidunt officiis dicta molestiae id maiores ducimus quis 
+//  distinctio nisi dolor consequatur non vel, est autem, aliquid inventore doloribus atque aliquam. Similique 
+//  sequi placeat soluta, tenetur dolorum enim sint ullam maxime consectetur eligendi vel totam?");
+
+console.log("O'zbekiston go'zal davlat");
+
+var paket = "Olma, banan, apelsin, cola, mandarin, uzum, tarvuz";
+
+console.log(paket);
+
+// var ism = "Anvar";
+// var familiya = "Anvarov";
+// console.log(ism);
+// let a = 6;
+// let b = 7;
+// let c = a + b;
+// console.log(c);
+// let mening_dasturim = '';
+// let MeningDasturim = '';
+// let meningDasturim = '';
+// let a = '';
+// let A = '';
+// let s = "12";
+// let name = "a'lo";
+
+
+// let Name = 'jdfs;alk';
+// let Lastname = 'ldskjfa';
+// let age = '3564';
+
+// console.log(Name);
+// console.log(Lastname);
+// console.log(age);
+
+// console.log(Name, Lastname, age);
+
+// Name = 'qwert';
+// let favoriteNumber = 888;
+// let numb = favoriteNumber / 2;
+// let michelsFavorite = 13;
+// michelsFavorite = 7;
+// favoriteNumber = michelsFavorite;
+// favoriteNumber = favoriteNumber * 26;
+
+// let a = "32"; // text yoki string
+// let b = 23; // number(int double float) yoki son
+
+// let a = 4;
+// let b = 5;
+// let c = a + b;
+// let g = a - b;
+// let h = a * b;
+// let o = a / b;
+// let r = a % b;
+
+// console.log("a + b = ", c);
+// console.log("a - b = ", g);
+// console.log("a * b = ", h);
+// console.log("a / b = ", o);
+// console.log("a % b = ", r);
+// console.log("a ** 3 = ", a ** 3);
+// console.log("b ** 3 = ", b ** 3);
+
+// let tx1 = "3";
+// let tx2 = "4";
+
+// let c = tx1 + tx2;
+// let g = tx1 - tx2;
+// let h = tx1 * tx2;
+// let o = tx1 / tx2;
+// let r = tx1 % tx2;
+
+// console.log("tx1 + tx2 = ", c);
+// console.log("tx1 - tx2 = ", g);
+// console.log("tx1 * tx2 = ", h);
+// console.log("tx1 / tx2 = ", o);
+// console.log("tx1 % tx2 = ", r);
+// console.log("tx1 ** 3 = ", tx1 ** 3);
+// console.log("tx1 ** 3 = ", tx2 ** 3);
+
+// let a=3, b=6, c=9;
+
+// let a = 4,
+//   b = 5,
+//   c = 5,
+//   r = 3;
+
+// let a = 4;
+// let b = 5;
+// let c = 5;
+let a, s, d, f, g, h, j;
+a = 5;
+s = 4;
+d = 4;
+f = 4;

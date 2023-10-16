ee7a18d10f461be3b166aa12cb0f87c66b024985
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-16 16:06:48 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert('Hello, JavaScript');
+let email = "akhrorweb@gmail.com";
+console.log(email);
+
+// string concatenation
+let firstName = "Ahror";
+let lastName = "Soliyev";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string Length => uzunlik
+console.log(fullName.length);
+
+// string methods => funktsiyaga o'xshash ish bajaradi (ob'yekt ichidagi funktsiya)
+console.log(fullName.toUpperCase());
+
+console.log(fullName.toLowerCase());
+
+console.log(email.indexOf('@'));

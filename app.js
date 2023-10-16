85158c5e22efe8e63daac8cb09245540569d601a
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-16 16:19:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// funktion decloration
+
+sayHello();
+sayHello();
+sayHello();
+
+function sayHello() {
+    console.log("Hello Function decloration");
+}
+
+// function expression
+const sayByeBye = function() {
+    console.log("SayByeBye function expression");
+}
+sayByeBye();
+
+// arrow function
+const nice = () => {
+    console.log("This is arrow function");
+}
+nice();

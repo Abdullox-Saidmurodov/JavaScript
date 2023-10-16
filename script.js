89fa20a4e6bbf1790428c1db7a9367dd4058c209
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-16 16:01:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// boolean and condation
+// let a = 10;
+// let name = "ali";
+// let b = 10;
+// let bl = true;
+// let bn = false;
+// console.log(a == b);
+// console.log(a < b);
+// console.log(a > b);
+// console.log(a != b);
+// console.log(a >= b);
+// console.log(a <= b);
+// let password = "12345";
+// // let newpass = "1234";
+// let newpass = prompt("Parolni kiriting! ");
+// alert(password == newpass);
+
+// if (password == newpass) {
+//     alert("Xush kelibsiz!");
+// }
+
+// let yosh = prompt("Yoshingiz nechchida? ");
+// if (yosh >= 16) {
+//     alert("Passport olasiz");
+// }
+// else {
+//     alert("Yoshingiz yetmaydi");
+// }
+
+// let son = prompt("Son kiriting? ");
+// if (son > 20) {
+//     alert("Son 20dan katta");
+// }
+// else if (son < 20) {
+//     alert("Son 20dan kichkina");
+// }
+
+let s1 = prompt("1-soni kiriting ");
+let s2 = prompt("2-soni kiriting ");
+if (s1 == s2) {
+    alert("Sonlar teng");
+} else {
+    alert("Sonlar teng emas");
+}

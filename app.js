d0c29619cc52e0ed83b411b0922ba2ad2c7032c6
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-16 16:20:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// Functions vs methods
+
+const ism = "ahror";
+
+// function
+
+const great = () => "Hello";
+
+const result = great();
+console.log(result)
+
+// methods
+
+const res = ism.toUpperCase();
+console.log(res);

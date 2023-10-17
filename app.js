d75e8b032bc1786c0a1d42cda5afe247e999f0f7
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-17 09:44:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,165 @@
+// boolean and condation
+let num1 = 5;
+let num2 = 5;
+// alert(num1 < num2);
+// alert(num1 == num2);
+// alert(num1 != num2);
+// let elogin = "admin";
+// let login = prompt("Loginingizni kiriting: ")
+// let password = prompt("Parolni kiriting: ")
+// if(login == elogin && password == "12345") {
+//     alert("hello admin");
+// } else {
+//     alert("Xato qildingiz");
+// }
+// let day = prompt("Hafta kunini kiriting: ");
+// if (day == 1) {
+//     alert("Dushanba");
+// } else if (day == 2) {
+//     alert("Seshanba");
+// } else if (day == 3) {
+//     alert("Chorshanba");
+// } else if (day == 4) {
+//     alert("Payshanba");
+// } else if (day == 5) {
+//     alert("Juma");
+// } else if (day == 6) {
+//     alert("Shanba");
+// } else if (day == 7) {
+//     alert("Yakshanba");
+// } else {
+//     alert("Bunaqa hafta kuni yo'q");
+// }
+
+// let a = 5;
+// a > 4 ? alert("Katta") : alert("Kichkina");
+
+/////////////
+
+// let a = prompt("1-sonni kiriting: ");
+// let b = prompt("2-sonni kiriting: ");
+
+// if(a > b) {
+//     alert(a);
+// } else if(a < b) {
+//     alert(b);
+// }
+
+// let k = prompt("Baho kiriting: ");
+
+// if(k == 1) {
+//     alert("Yomon");
+// } else if(k == 2) {
+//     alert("Qoniqarsiz");
+// } else if(k == 3) {
+//     alert("Qoniqarli");
+// } else if(k == 4) {
+//     alert("Yaxshi");
+// } else if(k == 5) {
+//     alert("A'lo");
+// } else {
+//     alert("Xato");
+// }
+
+///////////
+
+// let kun = prompt("Oy raqamini kiriting: ");
+
+// if(kun == 1 || kun == 2 || kun == 12) {
+//     alert("Qish");
+// } else if(kun == 3 || kun == 4 || kun == 5) {
+//     alert("Bahor");
+// } else if(kun == 6 || kun == 7 || kun == 8) {
+//     alert("Yoz");
+// } else if(kun == 9 || kun == 10 || kun == 11) {
+//     alert("Kuz");
+// } 
+
+///////////
+
+// let x = 5;
+// let kun;
+// switch(x) {
+// case 1:
+//     kun = "Dushanba";
+//     break;
+// case 2:
+//     kun = "Seshanba";
+//     break;
+// case 3:
+//     kun = "Chorshanba";
+//     break;
+// case 4:
+//     kun = "Payshanba";
+//     break;
+// case 5:
+//     kun = "Juma";
+//     break;
+// case 6:
+//     kun = "Shanba";
+//     break;
+// case 7:
+//     kun = "Yakshanba";
+//     break;
+// default:
+//     kun = "Xato";
+//     break;
+// }
+// alert(kun);
+
+////////////
+
+// let raqam = prompt("Oy raqamini kiriting: ");
+
+// if(raqam == 1 || raqam == 3 || raqam == 5 || raqam == 7 || raqam == 8 || raqam == 10 || raqam == 12) {
+//     alert("31 kun");
+// } else if(raqam == 4 || raqam == 6 || raqam == 9 || raqam == 11) {
+//     alert("30 kun");
+// } else if(raqam == 2) {
+//     alert("28 yoki 29 kun");
+// } else {
+//     alert("Bunday oy raqami yo'q");
+// }
+
+// //////////////////
+
+// let a = prompt("1-sonni kiriting: ");
+// let b = prompt("2-sonni kiriting: ");
+// let x = prompt("tartib sonni kiriting: ");
+
+// if(x == 1) {
+//     alert(a + b);
+// } else if(x == 2) {
+//     alert(a - b);
+// } else if(x == 3) {
+//     alert(a / b);
+// } else if(x == 7) {
+//     alert(a * b);
+// }
+
+//////////////////////
+
+// let a = 1;
+// if(a > 0) {
+//     a = a + 1;
+//     alert(a);
+// } else {
+//     alert(a);
+// }
+
+////////////////////
+
+let a = +prompt("Son kiriting: ");
+
+if(a > 0) {
+    a = a + 1;
+    alert(a);
+} else {
+    a = a - 2;
+    alert(a);
+}
+
+// if(a > 0) alert(a + 1)
+// else if(a < 0) alert(a - 2);
+
+// a > 0 ? alert(a + 1) : alert(a - 2);

8d6edf64605930c2c7fe3f160228176c27785b47
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-18 16:09:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// Math object
+
+// Math, Math.PI, Math.E
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+// const area = 7.9;
+// const area = 7.9;
+// const area = -7.1;
+// const area = -7.9;
+
+// sonni eng yaqin butun qismiga yaxlitlaydi
+// Math.round()
+// console.log(Math.round(area));
+
+// eng yaqin kichik butun qismiga qarab yaxlitlaydi
+// Math.floor()
+// console.log(Math.floor(area));
+
+// eng yaqin kattasining butun qismiga qarab yaxlitlaydi
+// Math.ceil()
+// console.log(Math.ceil(area));
+
+// sonning faqat butun qismini qaytaradi
+// Math.trunc()
+// console.log(Math.trunc(area));
+
+// 0 va 1 oralig'ida istalgan butun sonni qaytaradi
+// Math.random()
+console.log(Math.random());
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// .isArray();

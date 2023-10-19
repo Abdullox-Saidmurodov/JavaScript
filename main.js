043b0c79d719dbb99284de0f921f765b580a139c
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-10-19 11:32:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,237 @@
+// condation
+// switch
+
+// let a = 4;
+// if(a > 0) {
+//     alert("Musbat son");
+// }
+
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+
+// if(a > 2 && b <= 3) {
+//     alert("rost");
+// } else {
+//     alert("yolg'on");
+// }
+
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+
+// if(a >= 0 || b < -2) {
+//     alert("rost");
+// } else {
+//     alert("yolg'on");
+// }
+
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+// let c = +prompt("3-sonni kiriting: ");
+
+// if(a <= b && b <= c) {
+//     alert("rost");
+// } else {
+//     alert("yolg'on");
+// }
+
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+// let c = +prompt("3-sonni kiriting: ");
+
+// if(a < b && b < c) {
+//     alert("rost");
+// } else {
+//     alert("yolg'on");
+// }
+
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+
+// if((a % 2) && (b % 2)) {
+//     alert("rost");
+// } else {
+//     alert("yolg'on");
+// }
+
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+
+// if(((a % 2) || (b % 2)) && a != b) {
+//     alert("rost");
+// } else {
+//     alert("yolg'on");
+// }
+
+/////////////////////////////////////////////////////////////////////////////
+
+// switch
+
+// let n = +prompt("Tartib raqamini kiriting: ");
+// switch(n) {
+//     case (n = 1): alert("Ali");
+//     break;
+//     case (n = 2): alert("Zafar");
+//     break;
+//     case (n = 3): alert("Zarshed");
+//     break;
+//     default: alert("Bunaqa tartib raqamli o'quvchi yo'q.");
+//     break;
+// }
+
+// let n = +prompt("Tartib raqamini kiriting: ");
+// switch(n) {
+//     case 1: alert("Ali");
+//     break;
+//     case 2: alert("Zafar");
+//     break;
+//     case 3: alert("Zarshed");
+//     break;
+//     default: alert("Bunaqa tartib raqamli o'quvchi yo'q.");
+//     break;
+// }
+
+// let n = +prompt("Tartib raqamini kiriting: ");
+// switch(n) {
+//     case 1:
+//     case 2:
+//     case 3: alert("Ali");
+//     break;
+// }
+
+// let x = 10;
+// let kun;
+// switch(x) {
+// case 1:
+//     kun = "Dushanba";
+//     break;
+// case 2:
+//     kun = "Seshanba";
+//     break;
+// case 3:
+//     kun = "Chorshanba";
+//     break;
+// case 4:
+//     kun = "Payshanba";
+//     break;
+// case 5:
+//     kun = "Juma";
+//     break;
+// case 6:
+//     kun = "Shanba";
+//     break;
+// case 7:
+//     kun = "Yakshanba";
+//     break;
+// default:
+//     kun = "Xato";
+//     break;
+// }
+// alert(kun);
+
+// let k = 10;
+// switch(k) {
+//     case 1: alert("Yomon");
+//     break;
+//     case 2: alert("Qoniqarsiz");
+//     break;
+//     case 3: alert("Qoniqarli");
+//     break;
+//     case 4: alert("Yaxshi");
+//     break;
+//     case 5: alert("A'lo");
+//     break;
+//     default: alert("Xato");
+//     break;
+// }
+
+// let k = 2;
+// switch(k) {
+//     case 1: 
+//     case 2: 
+//     case 12: alert("Qish");
+//     break;
+//     case 3: 
+//     case 4: 
+//     case 5: alert("Bahor");
+//     case 6: 
+//     case 7: 
+//     case 8: alert("Yoz");
+//     case 9: 
+//     case 10: 
+//     case 11: alert("Qish");
+//     default: alert("Xato");
+//     break;
+// }
+
+// let k = 88;
+// switch(k) {
+//     case 1: 
+//     case 3: 
+//     case 5: 
+//     case 7: 
+//     case 8: 
+//     case 10:
+//     case 12: 
+//     case 8: alert("31 kun");
+//         break;
+//     case 4: 
+//     case 6: 
+//     case 9: 
+//     case 11: alert("30 kun");
+//         break;
+//     case 2: alert("28 yoki 29 kun");
+//         break;
+//     default: alert("Xato");
+//     break;
+// }
+
+// let a = 5;
+// let b = 9;
+// let k = 3;
+// switch(k) {
+//     case 1: alert(a + b);
+//         break;
+//     case 2: alert(a - b);
+//         break;
+//     case 3: alert(a * b);
+//         break;
+//     case 3: alert(a / b);
+//         break;
+//     default: alert("Xato");
+//     break;
+// }
+
+// let a = 6546;
+// let k = 4;
+// switch(k) {
+//     case 1: alert(a / 10);
+//         break;
+//     case 2: alert(a * 1000);
+//         break;
+//     case 3: alert(a);
+//         break;
+//     case 4: alert(a / 1000);
+//         break;
+//     case 5: alert(a / 100);
+//         break;
+//     default: alert("Xato");
+//     break;
+// }
+
+let a = 1000;
+let k = 4;
+switch(k) {
+    case 1: alert(a);
+        break;
+    case 2: alert(a * 1000000);
+        break;
+    case 3: alert(a * 1000);
+        break;
+    case 4: alert(a / 1000);
+        break;
+    case 5: alert(a / 100);
+        break;
+    default: alert("Xato");
+    break;
+}
+

bd37a7ee63b4a635d345afa022d2cb9958576796
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-11-03 16:13:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,132 @@
+// // Array yoki massiv
+// let a = 12;
+// let b = 13;
+// let c = 15;
+// let ms = [12, 13, 15, "ali", "vali", true, "122", 1234];
+// console.log(ms[3]);
+// console.log(ms[0]);
+// console.log(ms[9]);
+
+// let dust = ["Ali", "Vali", "Ahror", "Sardor", "Doniyor"];
+// dust.push("Hoshim");
+// dust.push("Arslon");
+// dust.push("Rustam");
+// dust.push("Dilshod");
+// dust.push("Abror");
+// for(let i = 0; i < 10; i++) {
+//     console.log(dust[i]);
+// }
+// let dust_ = ["Nozim", "Ilhom", "Kamol"];
+// let u_dust = dust.concat(dust_);
+// console.log(u_dust);
+
+
+
+// // 11/01/2023
+// let students = [];
+// // 11/02/2023
+// console.log(students);
+// students.push("Shoxrux");
+// students.push("Ali");
+// // Sanjar Aziza Odina Akbar Umida
+// students.push("Sanjar");
+// students.push("Aziza");
+// students.push("Odina");
+// students.push("Akbar");
+// students.push("Umida");
+
+// let A = ["Ali", "akbar", "umida"];
+// let B = ["Zafar", "maqsud", "aziz"];
+// let C = A.concat(B);
+// console.log(C);
+
+// let aclass = ["aziz", "laziz", "fariz", "ali", "odina"];
+// aclass[1] = "Sarvar";
+// console.log(aclass);
+// aclass.pop();
+// console.log(aclass);
+// aclass.push("Umida");
+// aclass.unshirt("Malika");
+
+
+// let dust = ["Ali", "Vali", "Ahror", "Sardor", "Doniyor"];
+// dust.push("Hoshim");
+// dust.push("Arslon");
+// dust.push("Rustam");
+// dust.push("Dilshod");
+// dust.push("Abror");
+
+// let dust_ = ["Nozim", "Ilhom", "Kamol"];
+// let u_dust = dust.concat(dust_);
+
+// for(let i = 0; i < 13; i++) {
+//     u_dust.pop();
+// }
+// console.log(u_dust);
+
+// let a = [1, 3, 5, 2, 5, 4, 6, 7];
+// let c = a.reverse();
+// console.log(c);
+// let b = a.sort();
+// console.log(b);
+// console.log(a.shift());
+// console.log(a.length);
+
+// let a = [1, 3, 5, 2, 5, 4, 6, 7];
+// console.log(a.slice(2, 5));
+// for(i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+
+
+
+// let ruyxat = ["O'zbekiston", "Tojikiston", "Turkmaniston", "Qirg'iziston", "Qozog'iston", "Turkiya", "Eron"];
+// for(i = 0; i < ruyxat.length; i++) {
+//     console.log(ruyxat[i]);
+// }
+
+
+// let ruyxat = ["O'zbekiston", "Tojikiston", "Turkmaniston", "Qirg'iziston", "Qozog'iston", "Turkiya", "Eron"];
+// console.log(ruyxat.length);
+
+// let a = [1, 3, 5, 2, 5, 4, 6, 7];
+// // console.log(a.sort());
+
+// // console.log(a.sort().reverse());
+
+// // console.log(a);
+
+// console.log(a.reverse());
+
+
+// let ruyxat = ["O'zbekiston", "Tojikiston", "Turkmaniston", "Qirg'iziston", "Qozog'iston", "Turkiya", "Eron"];
+// console.log(ruyxat.sort());
+// console.log(ruyxat.sort().reverse());
+
+// let a = [];
+// for(let i = 120; i <= 1200; i++) {
+//     if(i % 2 == 0) a.push(i);
+// }
+// console.log(a);
+
+// let a = [1, 3, 5, 2, 5, 4, 6, 7];
+// let b = 0;
+// for(let i = 0; i < a.length; i++) b+=a[i];
+// console.log(b);
+
+// // let c = Math.max(a);
+// console.log(Math.max(1, 3, 5, 2, 5, 4, 6, 7) - Math.min(1, 3, 5, 2, 5, 4, 6, 7));
+
+// let ruyxat = ["O'zbekiston", "Tojikiston", "Turkmaniston", "Qirg'iziston", "Qozog'iston", "Turkiya", "Eron"];
+// console.log(ruyxat.length);
+
+// let a = [1, 3, 5, 2, 5, 4, 6, 7, 1, 3, 5, 2, 5, 4, 6, 7, 1, 3, 5, 2, 5, 4, 6, 7, 1, 3, 5, 2, 5, 4, 6, 7, 1, 3, 5, 2, 5, 4, 6, 7];
+// console.log(a.slice(0, 6), a.slice(a.length/2, (a.length/2 + 8)), a.slice((a.length - 6), a.length));
+
+// let taomlar = [];
+// taomlar.push("Osh");
+// taomlar.push("Shashlik");
+// taomlar.push("Somsa");
+// taomlar.push("Qazi");
+// taomlar.push("XotDog");
+// console.log(taomlar);

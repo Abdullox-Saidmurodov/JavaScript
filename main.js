4ce59fa0e9b93e7a0e670345f2c54344e13c2356
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-11-10 16:06:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+// array
+// let arr = [2, 3, 4, 5, 6, 7];
+// console.log(arr[0]);
+// let arr = [
+//     [1, 2, 3],
+//     [1, 2, 3, ["ali", "zafar", ["klsd", ["lkdaf", "d;jla"]]]],
+//     [1, 2, 3],
+// ]
+// console.log(arr);
+
+// let arr = ["zafar", "hasan", "ilhom"];
+// console.log(arr.indexOF("1"));
+
+// let array = ["Doston", "Sobir", "Doniyor", "Doston", "Sobir", "Doniyor", "Doston", "Sobir", "Doniyor", "Alisher" ];
+
+// let gg = array.map((e) => {
+//     return e;
+// })
+// console.log(gg);
+
+// array.map((e, i) => {
+//     console.log(e, i);
+// })
+
+// let arr = [56,5,5,7,4,1,3,66,74,9,2,1,3,4,5,6,7,1];
+// let g = arr.filter(e => e > 5);
+// console.log(g);
+
+
+// 1-
+let ismlar = ["Hayot", "Ahmad", "Umid"];
+console.log(`Salom ${ismlar[0]}, ishlaring yaxshimi? `);
+console.log(`${ismlar[1]}, uydamisan? `);
+console.log(`Qayerdasan ${ismlar[0]}?, qachon ishdan qaytasan? `);
+
+// 2-
+let sonlar = [5,-9,45,-7,-7,5.4,-14.5];
+sonlar[5] += sonlar[0];
+sonlar[1] = 2;
+console.log(sonlar);
+
+// 3-
+let t_shaxslar = ["Amir Temur", "Islom Karimov"];
+let z_shaxslar = ["ljsad", "lksjad;flkjl;klkasdf"];
+
+console.log(t_shaxslar);
+console.log(z_shaxslar);
+
+// 4-
+console.log(`Men tarixiy shaxslardan ${t_shaxslar.pop()} bilan, zamonaviy shaxslardan ese ${z_shaxslar.pop()} bilan suhbat qilishni istar edim.`)
+
+// 5-
+let friends = [];
+let d = ["sdlka", "lkjas"];
+friends.push(d);
+console.log(friends);
+
+// 6-
+friends.remove("lkjas");
+console.log(friends);

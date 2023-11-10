3ad697183f67d06061db818a50416d9f0ecd716e
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-11-10 16:07:45 +0500

    Add files via upload

---
+++ home_work_1.js
@@ -0,0 +1,56 @@
+// // 1-
+// let a = 4, p;
+// p = 4 * a;
+// console.log(p);
+
+// // 2-
+// let a = 4, s;
+// s = 4 * a;
+// console.log(s);
+
+// // 3-
+// let a = 3, b = 2, s, p;
+// s = a * b;
+// p = 2 * (a + b);
+// console.log(`Yuzi: ${s}, Perimetri: ${p}`);
+
+// // 4-
+// let d = 4, po = 3.14, l;
+// l = pi * d;
+// console.log(l);
+
+// // 5-
+// let a = 8, v, s;
+// v = a**3;
+// s = 6 * (a**2);
+// console.log(`Hajmi: ${v}, Yuzi: ${s}`);
+
+// // 6-
+// let a = 2, b = 5, c = 3, v, s;
+// v = a * b * c;
+// s = 2 * (a * b + b * c + a * c);
+// console.log(`Hajmi: ${v}, Yuzi: ${s}`);
+
+// // 7-
+// let r = 3, pi = 3.14, l, s;
+// l = 2 * pi * r;
+// s = pi * (r**2);
+// console.log(`Uunligi: ${l}, Yuzi: ${s}`);
+
+// // 8-
+// let a = 3, b = 4;
+// console.log((a + b) / 2);
+
+// // 9-
+// let a = 3, b = 4;
+// console.log((a * b) ** (1 / 2));
+
+// // 10-
+// let a = 3, b = 4;
+// console.log(`Yig'indi: ${a + b}, Ko'paytma: ${a * b}, Kvadratlari: ${a ** 2}, ${b ** 2}`);
+
+// // 11-
+// let a = 3, b = 4;
+// console.log(`Yig'indi: ${a + b}, Ko'paytma: ${a * b}, Modullari: ${Math.abs(a)}, ${Math.abs(b)}`);
+
+// 12-
---
+++ main.js
@@ -0,0 +1,164 @@
+// 2 1 4
+// 1 2 4
+
+// // SortIcn3(5, 2, 1) 1 2 5
+// function SortInc3(a, b, c) {
+//     if(a > b && b > C) console.log
+// }
+
+
+// // 1-
+// let a = 2, b = 5, c = 1, d = 4, e = 7;
+// function powerA3 () {
+//     console.log(`${a} sonnining 3-darajasi: ${a**3}`);
+//     console.log(`${b} sonnining 3-darajasi: ${b**3}`);
+//     console.log(`${c} sonnining 3-darajasi: ${c**3}`);
+//     console.log(`${d} sonnining 3-darajasi: ${d**3}`);
+//     console.log(`${e} sonnining 3-darajasi: ${e**3}`);
+// }
+// powerA3();
+
+
+// // 2-
+// let a = 2, b = 5, c = 1;
+// function powerA234 (aa, bb, cc) {
+//     console.log(`${aa} sonnining kvadrati: ${aa**2}, 3-darajasi: ${aa**3}, 4-darajasi: ${aa**3}`);
+//     console.log(`${bb} sonnining kvadrati: ${bb**2}, 3-darajasi: ${bb**3}, 4-darajasi: ${bb**3}`);
+//     console.log(`${cc} sonnining kvadrati: ${cc**2}, 3-darajasi: ${cc**3}, 4-darajasi: ${cc**3}`);
+// }
+// powerA234(a, b, c);
+
+
+// // 3-
+// let a = 2, b = 5, c = 1, d = 4;
+// function mean (aa, bb, cc, dd) {
+//     console.log(`${aa} va ${bb} sonnining o'rta arifmetigi: ${(aa+bb)/2}, ${aa} va ${bb} sonnining o'rta geometrigi: ${aa*bb/2}`);
+//     console.log(`${aa} va ${cc} sonnining o'rta arifmetigi: ${(aa+cc)/2}, ${aa} va ${cc} sonnining o'rta geometrigi: ${aa*cc/2}`);
+//     console.log(`${aa} va ${dd} sonnining o'rta arifmetigi: ${(aa+dd)/2}, ${aa} va ${dd} sonnining o'rta geometrigi: ${aa*dd/2}`);
+// }
+// mean(a, b, c, d);
+
+
+// // 4-
+// let a = 2;
+// function mean (aa) {
+//     console.log(`Tomoni ${aa} bo'lgan teng tomonli uchburchakning perimetri: ${3*aa}`);
+//     console.log(`Tomoni ${aa} bo'lgan teng tomonli uchburchakning yuzi: ${((3**(1/2))/4)*(aa**2)},`);
+// }
+// mean(a);
+
+
+// // 5-
+// let x1 = 0;
+// let y1 = 0;
+// let x2 = 3;
+// let y2 = 4;
+// function RectPS(x1_, y1_, x2_, y2_){
+// let a = Math.abs(x2_ - x1_);
+// let b = Math.abs(y2_ - y1_);
+// console.log(`To'g'ri to'rtburchakning yuzi: ${a * b}`);
+// console.log(`To'g'ri to'rtburchakning perimetri: ${2 * (a + b)}`);
+// }
+// RectPS(x1, y1, x2, y2);
+
+
+// // 6-
+// let a = 54;
+// let b = 32;
+// let c = 9800;
+// function DigitCountSum(a_, b_, c_){
+
+// }
+
+
+// // 16-
+// let a = 4;
+// let b = -3;
+// function ishora(x){
+//     if(x > 0) {
+//         console.log(1);
+//         return 1;
+//     } else if(x < 0) {
+//         console.log(-1);
+//         return -1;
+//     } else {
+//         console.log(-1);
+//         return -1;
+//     }
+// }
+// console.log(ishora(a) + ishora(b));
+
+
+// // 24-
+// let a = 54;
+// let b = 33;
+// let c = 9800;
+// function Even(k) {
+//     if(!(k % 2)) return true;
+//     else if(k % 2) return false;
+// }
+// console.log(`1-son: ${Even(a)}`)
+// console.log(`2-son: ${Even(b)}`)
+// console.log(`3-son: ${Even(c)}`)
+
+
+// // 34-
+// let a = 5;
+// let b = 3;
+// let c = 10;
+// let x;
+// function Fact(n) {
+//     x = 1;
+//     for(let i = 1; i <= n; i++) {
+//         x *= i;
+//     }
+//     return x;
+// }
+// console.log(`1-son: ${Fact(a)}`);
+// console.log(`2-son: ${Fact(b)}`);
+// console.log(`3-son: ${Fact(c)}`);
+
+
+// // 35-
+// let a = 2;
+// let b = 4;
+// let c = 5;
+// let x;
+// function Fact2(n) {
+//     x = 1;
+//     for(let i = 1; i <= n; i++) {
+//         x *= i;
+//     }
+//     n = 1;
+//     for(let i = 1; i <= x; i++) {
+//         n *= i;
+//     }
+//     return n;
+// }
+// console.log(`1-son: ${Fact2(a)}`);
+// console.log(`2-son: ${Fact2(b)}`);
+// console.log(`3-son: ${Fact2(c)}`);
+
+
+// // 37-
+// let a1 = 5;
+// let a2 = 3;
+// let a3 = 10;
+// let b = 2;
+// function Power1(a, b) {
+//     return a**b;
+// }
+// console.log(`1-son: ${Power1(a1, b)}`);
+// console.log(`2-son: ${Power1(a2, b)}`);
+// console.log(`3-son: ${Power1(a3, b)}`);
+
+
+// // 50-
+// let h_ = 5;
+// let m_ = 3;
+// let s_ = 10;
+// let t_ = 2;
+// function TimeToHMS(h, m, s, t){
+//     console.log(`${t % 3600  }`)
+// }
+// console.log(new Date(400).toTimeString());

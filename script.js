22a1fcd9aabd269bdcb50b39ba8ee097621ed4fc
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-11-17 16:06:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// // string
+// let text = "Uzbekistan";
+// let tx = 'Samarqand';
+// let str = "A'lo";
+// let big = tx.toUpperCase();
+// let lit = text.toLowerCase();
+// console.log(big, lit, text);
+
+
+// let obj = {
+//     title: "Nexia",
+//     getInfo: function() {
+//         console.log(this.title);
+//     }
+// }
+
+// let tx = "Salom world";
+// let tx2 = tx.toLocaleLowerCase();
+// console.log(tx2);
+// s = 5;
+// console.log(s);
+
+// let tx = "uzbekistan poytaxti toshkent";
+// let tx2 = tx.slice(1, 12);
+// console.log(tx2);
+
+// let tx = "uzbekistan poytaxti toshkent uzbekistan";
+// let tx2 = tx.replace("uzbekistan", "O'zbekiston");
+// let tx3 = tx.replaceAll("uzbekistan", "O'zbekiston");
+// let tx4 = tx.replace("123", "O'zbekiston");
+// console.log(tx2);
+// console.log(tx3);
+// console.log(tx4);
+
+// let text = "Ali, Zafar, Ahmad, Dilshod";
+// let tx = text.split(",");
+// console.log(tx);
+
+// let text = "hello";
+// console.log(text[0], text[1]);
+
+// let text = "hello Uz";
+// let rt = text.charAt(4);
+// console.log(rt);
+
+// let text = "hello Uz";
+// let text1 = "jl;asdkj"
+// let rt = text.concat(text1);
+// let add = text + text1;
+// console.log(rt);
+// console.log(add);
+
+// let text = "hello world o";
+// console.log(text.indexOf("worl"));
+// console.log(text.lastIndexOf("o"));
+// console.log(text.startsWith("he"));
+// console.log(text.startsWith("worl"));
+// console.log(text.includes("rl"));
+
+// let num = 19;
+// let text = `Mening yoshim ${num} da`;
+// console.log(text);
+

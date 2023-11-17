7964ef1aab02aeffa1373c483bb6d74e1f81b6a9
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-11-17 16:07:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+// let num = 354154;
+// let num2 = 23.23;
+// let a = num.toString();
+// console.log(typeof a);
+
+// let str = "515621332";
+// let num = parseInt(str);
+// let float = parseFloat(str);
+// console.log(typeof str, typeof num);
+
+// let str = "32131df321df12";
+// console.log(parseInt(str));
+
+// let a = Math.sqrt(16);
+// console.log(a);
+// console.log(Math.pow(2, 3));
+
+// let number = Math.ceil(4.3);
+// console.log(number); 
+// // Output: 5
+
+// let number = 38.8;
+// // round number to nearest smallest number
+// let roundedNumber = Math.floor(number);
+// console.log(roundedNumber);
+// // Output: 38
+
+// let numbers = Math.max(12, 4, 5, 9, 0, -3);
+// console.log(numbers); 
+// // Output:  12
+
+// let numbers = Math.min(12, 4, 5, 9, 0, -3);
+// console.log(numbers); 
+// // Output:  -3
+
+// let randomNumber = Math.random()
+// console.log(randomNumber)
+// // Output: 0.16668531572829082
+
+// let number = 3.87;
+// // round the number to nearest integer
+// let roundedNumber = Math.round(number);
+// console.log(roundedNumber);
+// // Output: 4
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+
+// let kucha = "qwer";
+// let mahalla = ";lkj";
+// let tuman = "vcxz";
+// let viloyat = "asdf";
+// console.log(`* ${kucha} ko'chasi,`);
+// console.log(`  ${mahalla} mahallasi,`);
+// console.log(`  ${tuman} tumani,`);
+// console.log(`  ${viloyat} viloyati`);
+
+// let kucha = prompt("Ko'changizni nomini kiriting:");
+// let mahalla = prompt("Mahallangizni nomini kiriting:");
+// let tuman = prompt("Tumaningizni nomini kiriting:");
+// let viloyat = prompt("Viloyatingizni nomini kiriting:");
+// viloyat = viloyat.toUpperCase();
+// kucha = kucha.toLowerCase();
+// console.log(`* ${kucha} ko'chasi,`);
+// console.log(`  ${mahalla} mahallasi,`);
+// console.log(`  ${tuman} tumani,`);
+// console.log(`  ${viloyat} viloyati`);
+
+// let manzil = `${kucha} ko'chasi, ${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati`;
+// alert("manzil");
+

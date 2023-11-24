db49dd88b915977b6b6bdb174766bd4f3fb593a7
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-11-24 16:10:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,146 @@
+// // dom 2
+
+// let tag_ = document.getElementsByTagName('h1');
+// let class_ = document.getElementsByClassName('header');
+// let id_ = document.getElementById('head');
+
+// let tag_kq = document.querySelector('h1');
+// let class_q = document.querySelector('.header');
+// let id_q = document.querySelector('#head');
+
+// let h1 = document.querySelectorAll('h1');
+// console.log(h1[0]);
+// // h1[0].innerHTML = `<hr>`;
+// h1[1].innerHTML = `<button>hhh</button>`;
+// h1[2].innerText = `<hr>`;
+// h1[3].style.color = 'aqua'
+// h1[4].style.cssText = `
+
+// `
+///////////////////////////////////////////////////
+// let h2_1 = document.querySelector('#h_1');
+// let h2_2 = document.querySelector('#h_2');
+// let h2_3 = document.querySelector('#h_3');
+// let h2_4 = document.querySelector('#h_4');
+// let h2_5 = document.querySelector('#h_5');
+// let h2_6 = document.querySelector('#h_6');
+// let h2_7 = document.querySelector('#h_7');
+// let h2_8 = document.querySelector('#h_8');
+// let h2_9 = document.querySelector('#h_9');
+// let h2_10 = document.querySelector('#h_10');
+
+// h2_1.innerHTML = `<button>hhh</button>`;
+// h2_2.innerHTML = `<button>hhh</button>`;
+// h2_3.innerHTML = `<button>hhh</button>`;
+// h2_4.innerHTML = `<button>hhh</button>`;
+// h2_5.innerHTML = `<button>hhh</button>`;
+// h2_6.innerHTML = `<button>hhh</button>`;
+// h2_7.innerHTML = `<button>hhh</button>`;
+// h2_8.innerHTML = `<button>hhh</button>`;
+// h2_9.innerHTML = `<button>hhh</button>`;
+// h2_10.innerHTML = `<button>hhh</button>`;
+
+// h2_1.style.cssText = `
+// background-color: yellow;
+// `
+// h2_2.style.cssText = `
+// background: yellow;
+// `
+// h2_3.style.cssText = `
+// background: yellow;
+// `
+// h2_4.style.cssText = `
+// background: yellow;
+// `
+// h2_5.style.cssText = `
+// background: yellow;
+// `
+// h2_6.style.cssText = `
+// background: yellow;
+// `
+// h2_7.style.cssText = `
+// background: yellow;
+// `
+// h2_8.style.cssText = `
+// background: yellow;
+// `
+// h2_9.style.cssText = `
+// background: yellow;
+// `
+// h2_10.style.cssText = `
+// background: yellow;
+// `
+
+// let hh = document.querySelector('#head');
+// let parol = +prompt("Parolni kiriting");
+// if(parol == 12345){
+//     hh.innerHTML = "To'g'ri";
+//     hh.style.background = "green";
+// } else {
+//     hh.innerHTML = "Xato";
+//     hh.style.background = "red";
+// }
+
+// let hh = document.querySelector('#head');
+// let tartib_r = +prompt("Parolni kiriting");
+// switch(tartib_r){
+//     case 1: {
+//         hh.innerHTML = "Dushanba";
+//         hh.style.backgroundColor = "green";
+//     } break;
+//     case 2: {
+//         hh.innerHTML = "Seshanba";
+//         hh.style.background = "yellow";
+//     } break;
+//     case 3: {
+//         hh.innerHTML = "Chorshanba";
+//         hh.style.background = "blue";
+//     } break;
+//     case 4: {
+//         hh.innerHTML = "Payshanba";
+//         hh.style.background = "lightgreen";
+//     } break;
+//     case 5: {
+//         hh.innerHTML = "Juma";
+//         hh.style.background = "aqua";
+//     } break;
+//     case 6: {
+//         hh.innerHTML = "Shanba";
+//         hh.style.background = "gray";
+//     } break;
+//     case 7: {
+//         hh.innerHTML = "Yakshanba";
+//         hh.style.background = "lightyellow";
+//     } break;
+//     default: {
+//         hh.innerHTML = "Bunday hafta kuni yo'q";
+//         hh.style.background = "red";
+//     } 
+// }
+
+let hh = document.querySelector('#head');
+let hi = document.querySelector('#hi');
+let tartib_r = +prompt("Parolni kiriting");
+switch(tartib_r){
+    case 1: {
+        hh.innerHTML = "Dushanba";
+        hh.style.backgroundColor = "green";
+    } break;
+    case 2: {
+        hh.innerHTML = "Seshanba";
+        hh.style.background = "yellow";
+    } break;
+    case 3: {
+        hh.innerHTML = "Chorshanba";
+        hh.style.background = "blue";
+    } break;
+    case 4: {
+        hh.innerHTML = "Payshanba";
+        hh.style.background = "lightgreen";
+    } 
+    case 5: {
+        hh.innerHTML = "Juma";
+        hh.style.background = "aqua";
+    }
+    hi.innerHTML = "1234";
+}kkjgds

72d0faebc3bfb7a45526462435945205c5c5fd4e
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-11-27 16:08:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// let box = document.querySelector('.box');
+// console.log(box.classList);
+
+// function op() {
+//     box.classList.add('main');
+// }
+
+// function cl() {
+//     box.classList.remove('main');
+// }
+
+// function oc() {
+//     if(box.classList.contains('main')) {
+//         box.classList.remove('main');
+//     } else {
+//         box.classList.add('main');
+//     }
+// }
+
+// function t() {
+//     box.classList.toggle('main');
+// }
+
+let wv = document.querySelector('.wv');
+
+function o() {
+    wv.classList.remove('tg');
+}
+
+function c() {
+    wv.classList.add('tg');
+}
+
+// let x = document.querySelector('#jk');
+// console.log(x);

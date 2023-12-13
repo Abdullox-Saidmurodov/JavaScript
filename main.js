a7ffd397b2e3f0a295d845cbdf7b00ed4d01d222
Abdullox-Saidmurodov <146757953+Abdullox-Saidmurodov@users.noreply.github.com>
2023-12-13 17:09:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,406 @@
+// let btn = document.querySelector("button");
+// let h1 = document.querySelector("h1");
+// let p = document.querySelector("p");
+
+// h1.addEventListener("mouseover", () => {
+//     // p.innerHTML = ";lkjadf;laksdfj";
+//     h1.style.background = "red";
+//     h1.style.color = "green";
+
+//     setTimeout(() => {
+//         h1.style.background = "gold";
+//     },4000)
+// })
+// let d = 0;
+// setTimeout(() => {
+//     h1.style.background = "gold";
+//     if(d == 1) {
+//     h1.style.background = "yellow";
+//     }
+// },4000)
+// function click(){
+//     h1.innerHTML = "Bye";
+// }
+
+// h1.addEventListener("click", click)
+// btn.addEventListener("click", Click);
+
+let q1 = document.querySelector(".q1");
+let q2 = document.querySelector(".q2");
+let q3 = document.querySelector(".q3");
+let q4 = document.querySelector(".q4");
+let q5 = document.querySelector(".q5");
+let q6 = document.querySelector(".q6");
+let q7 = document.querySelector(".q7");
+let q8 = document.querySelector(".q8");
+let q9 = document.querySelector(".q9");
+
+q1.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q1.style.background = "black";
+    q1.innerHTML = "0";
+    q1.style.color = "white";
+})
+q2.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q2.style.background = "black";
+    q2.innerHTML = "0";
+    q2.style.color = "white";
+})
+q3.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q3.style.background = "black";
+    q3.innerHTML = "0";
+    q3.style.color = "white";
+})
+q4.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q4.style.background = "black";
+    q4.innerHTML = "0";
+    q4.style.color = "white";
+})
+q5.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q5.style.background = "black";
+    q5.innerHTML = "0";
+    q5.style.color = "white";
+})
+q6.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q6.style.background = "black";
+    q6.innerHTML = "0";
+    q6.style.color = "white";
+})
+q7.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q7.style.background = "black";
+    q7.innerHTML = "0";
+    q7.style.color = "white";
+})
+q8.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q8.style.background = "black";
+    q8.innerHTML = "0";
+    q8.style.color = "white";
+})
+q9.addEventListener("click", () => {
+    q1.style.background = "red";
+    q1.innerHTML = "1";
+    q1.style.color = "black";
+
+    q2.style.background = "green";
+    q2.innerHTML = "2";
+    q2.style.color = "black";
+
+    q3.style.background = "#1ff00c";
+    q3.innerHTML = "3";
+    q3.style.color = "black";
+
+    q4.style.background = "#a715a7";
+    q4.innerHTML = "4";
+    q4.style.color = "black";
+
+    q5.style.background = "#eb9898";
+    q5.innerHTML = "5";
+    q5.style.color = "black";
+
+    q6.style.background = "#3f0b32";
+    q6.innerHTML = "6";
+    q6.style.color = "black";
+
+    q7.style.background = "#7dc244";
+    q7.innerHTML = "7";
+    q7.style.color = "black";
+
+    q8.style.background = "#549191";
+    q8.innerHTML = "8";
+    q8.style.color = "black";
+
+    q9.style.background = "#8ffa04";
+    q9.innerHTML = "9";
+    q9.style.color = "black";
+
+    q9.style.background = "black";
+    q9.innerHTML = "0";
+    q9.style.color = "white";
+})
